b05b6081190f8b844884ed35de7b99e7493cdde4
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2022-03-26 16:48:07 -0300

    Add end-to-end test

--- src/test/java/br/com/italo/marsrovers/AppTest.java
+++ src/test/java/br/com/italo/marsrovers/AppTest.java
@@ -1,4 +1,15 @@
 package br.com.italo.marsrovers;
 
-public class AppTest {    
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	
+	@Test
+	public void testMainMethod() throws IOException, URISyntaxException {
+		App.main(null);
+	}
+	
 }
