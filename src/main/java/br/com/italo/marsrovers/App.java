cab25d8a5414d4ee459e794219b7bc479d91ff0a
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2022-03-26 07:38:54 -0300

    Read input file

--- src/main/java/br/com/italo/marsrovers/App.java
+++ src/main/java/br/com/italo/marsrovers/App.java
@@ -1,9 +1,17 @@
 package br.com.italo.marsrovers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
 	
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+    	List<String> lines = Files.readAllLines(Paths.get("input"));
+    	for (String line : lines) {
+			System.out.println(line);
+		}
     }
     
 }
